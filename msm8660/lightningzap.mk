0290df74317672b2f9f4879ec7afab9dae91b4c7
TeamFahQ <TeamFahQ1@gmail.com>
2013-10-19 10:29:43 -0500

    Updated lz.mk for msm8660 to point to the correct directory. Removed un-needed readme.

--- msm8660/lightningzap.mk
+++ msm8660/lightningzap.mk
@@ -4,15 +4,13 @@
 
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/lz-kernel/scripts/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/lz-kernel/scripts/etc/init.d/00lztweak:system/etc/init.d/00lztweak \
-    vendor/lz-kernel/scripts/etc/init.d/01governor:system/etc/init.d/01governor \
-    vendor/lz-kernel/scripts/etc/init.d/04vaccum_sqlite:system/etc/init.d/04vaccum_sqlite \
-    vendor/lz-kernel/scripts/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel \
-    vendor/lz-kernel/scripts/etc/init.d/89prop:system/etc/init.d/89prop \
-    vendor/lz-kernel/scripts/etc/init.d/90userinit:system/etc/init.d/90userinit \
+    vendor/lz-kernel/msm8660/scripts/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/lz-kernel/msm8660/scripts/etc/init.d/00lztweak:system/etc/init.d/00lztweak \
+    vendor/lz-kernel/msm8660/scripts/etc/init.d/01governor:system/etc/init.d/01governor \
+    vendor/lz-kernel/msm8660/scripts/etc/init.d/04vaccum_sqlite:system/etc/init.d/04vaccum_sqlite \
+    vendor/lz-kernel/msm8660/scripts/etc/init.d/89prop:system/etc/init.d/89prop \
 
 # Apps
 PRODUCT_COPY_FILES += \
-    vendor/lz-kernel/apps/RootBrowser.apk:system/app/RootBrowser.apk \
-    vendor/lz-kernel/apps/VoltageControl.apk:system/app/VoltageControl.apk \
+    vendor/lz-kernel/msm8660/apps/RootBrowser.apk:system/app/RootBrowser.apk \
+    vendor/lz-kernel/msm8660/apps/VoltageControl.apk:system/app/VoltageControl.apk \
