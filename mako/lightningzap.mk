c7ea998d0c4f1eb17af102ac14a573a2e2c9652e
thomas <teamfahq1@gmail.com>
2014-02-20 22:03:08 -0600

    Add mako. Updating msm8660

---
+++ mako/lightningzap.mk
@@ -0,0 +1,15 @@
+#This is intended to automatically add Lightning Zap scripts to your build 
+#This eliminates the steps of adding the init.d scripts and sysctl.conf to your already built .zip then regenerating a new md4sum
+#Team Fah-Q! thanks you for your support
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/lz-kernel/mako/scripts/etc/init.d/00lztweak:system/etc/init.d/00lztweak \
+    vendor/lz-kernel/mako/scripts/etc/init.d/01governor:system/etc/init.d/01governor \
+    vendor/lz-kernel/mako/scripts/etc/init.d/04vaccum_sqlite:system/etc/init.d/04vaccum_sqlite \
+    vendor/lz-kernel/mako/scripts/etc/init.d/89prop:system/etc/init.d/89prop \
+
+# Apps
+PRODUCT_COPY_FILES += \
+    vendor/lz-kernel/mako/apps/RootBrowser.apk:system/app/RootBrowser.apk \
+    vendor/lz-kernel/mako/apps/VoltageControl.apk:system/app/VoltageControl.apk \
